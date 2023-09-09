8f083b95908629994d36fe03ee021122cf50d7e5
noko1024 <ynoko1024@gmail.com>
2023-09-09 21:29:46 +0900

    試しに消してみる

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,7 +3,6 @@
 
 import type { NextRequest } from 'next/server'
 
-export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
   return new Response(JSON.stringify({ name: 'John Doe' }))
