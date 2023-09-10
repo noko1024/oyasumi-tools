c7f6325d2fb98fd932b955e1665734e79d850927
noko1024 <ynoko1024@gmail.com>
2023-09-10 19:36:05 +0900

    APIのテストコードを追加

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,8 +2,22 @@
 // Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
 
 import type { NextRequest } from 'next/server'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 
+import { withApiAuthRequired } from '@auth0/nextjs-auth0/edge';
+
+export const runtime = 'edge'
+
+export const GET = withApiAuthRequired(async function test(request: NextApiRequest, response: NextApiResponse) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
+
+})
+
+/*
 export async function GET(request: NextRequest) {
   return new Response(JSON.stringify({ name: 'John Doe' }))
 }
+
+*/
+
